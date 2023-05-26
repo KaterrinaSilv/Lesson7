7575cc165bbf36cf7338a738eb05c0113a8f132f
Katerina Silver <90252663+KaterrinaSilv@users.noreply.github.com>
2023-05-26 12:24:42 +0300

    Initial commit

---
+++ timeservice/src/main/java/ru/mirea/serebriakovaea/timeservice/MainActivity.java
@@ -0,0 +1,67 @@
+package ru.mirea.serebriakovaea.timeservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
+import ru.mirea.serebriakovaea.timeservice.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private final String host = "time.nist.gov";
+    private final int port = 13;
+    private final String TAG = getClass().getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnDatetime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetTimeTask timeTask = new GetTimeTask();
+                timeTask.execute();
+
+            }
+        });
+
+    }
+
+    private class GetTimeTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String timeResult = "";
+            try {
+                Socket socket = new Socket(host, port);
+                BufferedReader reader = SocketUtils.getReader(socket);
+                reader.readLine();
+                timeResult = reader.readLine();
+                Log.d(TAG, timeResult);
+                socket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return timeResult;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            binding.textDatetime.setText(s);
+        }
+    }
+}
